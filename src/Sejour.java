b04ac7698dce869fdd5ee2172c0babea1afc6ab9
chailloux.valentin <valentinchailloux7@gmail.com>
2022-09-13 11:37:17 +0200

    "first commit"

---
+++ src/Sejour.java
@@ -0,0 +1,18 @@
+public class Sejour extends OptionVoyage{
+
+    private int _NbNuit;
+    private double _PrixByNuit;
+
+    public Sejour(int NbNuit, double PrixByNuit){
+
+        this._NbNuit = NbNuit;
+        this._PrixByNuit = PrixByNuit;
+
+    }
+
+    public double PrixSejour(){
+
+    }
+
+
+}
